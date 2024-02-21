c73c11972724c6771145fe92605eb300a90e8438
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-02-22 00:50:33 +0530

    set

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,18 +13,18 @@ const SearchBar = () => {
   };
   
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center">
+    <form onSubmit={handleSubmit} className="flex flex-col pt-1 sm:flex-row items-center">
   <input
     type="text"
     placeholder="Search..."
     value={searchTerm}
     onChange={handleChange}
-    className="border text-black border-gray-400 rounded-md px-4 py-2 mb-2 sm:mb-0 sm:mr-2 focus:outline-none focus:border-blue-500"
+    className="border  text-black border-gray-400 sm:rounded-md sm:px-4 sm:py-2 mb-2 sm:mb-0 sm:mr-2 focus:outline-none focus:border-blue-500"
   />
   <Link href={`/searchProduct/${searchTerm}`}>
-    <button type="submit" className="sm:px-4 hidden sm:py-2 bg-blue-500 text-white sm:rounded-md">
-      Search
-    </button>
+  <button type="submit" className="hidden md:inline-block px-4 py-2 bg-blue-500 text-white rounded-md">
+  Search
+</button>
   </Link>
 </form>
   );
