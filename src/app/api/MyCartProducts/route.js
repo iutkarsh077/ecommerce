c3e7638f798e0c612003c037c5eb91a2f08ff630
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-02-29 09:43:38 +0530

    all set cobnsole logs are commented

--- src/app/api/MyCartProducts/route.js
+++ src/app/api/MyCartProducts/route.js
@@ -8,10 +8,10 @@ export async function GET() {
     await connectToDB();
     const user = await currentUser();
     const email = user.emailAddresses[0].emailAddress;
-    console.log("User Email:", email);
+    // console.log("User Email:", email);
 
     const cartItems = await MyCart.find({ Email: email }).maxTimeMS(30000);
-    console.log("Cart Items:", cartItems);
+    // console.log("Cart Items:", cartItems);
 
     if (cartItems && cartItems.length > 0) {
       return NextResponse.json({ status: true, msg: "Items from cart", cartItems });
